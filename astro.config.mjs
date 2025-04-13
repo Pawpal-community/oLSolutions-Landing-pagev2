c72ce79fa288aa960ecb8d539c45327d56523567
Luis Reyes <yuiftw05@gmail.com>
2025-04-13 15:42:48 -0600

    feat: sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hontechn.com/",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(),sitemap({sitemap: '/sitemap.xml'}), icon()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
